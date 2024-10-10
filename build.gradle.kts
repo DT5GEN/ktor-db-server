2bf60c44d84bc448756fa19afcdb9f5214786683
Alx_Shilov <deeppowercrew@gmail.com>
2024-10-11 02:34:44 +0300

    test routes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.21"
     id("io.ktor.plugin") version "3.0.0"
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 group = "com.dt5gen"
@@ -30,6 +30,11 @@ dependencies {
     implementation("io.ktor:ktor-serialization-gson-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-sessions:2.3.0")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/dt5gen/plugins/Routing.kt
+++ src/main/kotlin/com/dt5gen/plugins/Routing.kt
@@ -1,13 +1,42 @@
 package com.dt5gen.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
+        get("/notes") {
             call.respondText("Hello World!")
         }
+
+        get("/note/{id}") {
+            val id = call.parameters["id"]
+            call.respondText("$id")
+        }
+        get("/note") {
+            val id = call.request.queryParameters["id"]
+            call.respondText("$id")
+        }
+
+
+        route("/notes") {
+            route("/create") {
+                // http://192.168.0.243:8080/
+                post {
+                    // call.respondText("Something post")
+                    val body = call.receive<String>()
+                    call.respond(body)
+                }
+            }
+
+            delete {
+                val body = call.receive<String>()
+                call.respond(body)
+            }
+        }
+
+
     }
 }
