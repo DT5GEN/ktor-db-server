c56ef4ac4c2e00e8c0f2c6509be2ae71352eb6c2
Alx_Shilov <deeppowercrew@gmail.com>
2024-10-11 12:02:36 +0300

    test routes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val postgres_version: String by project
+val hikari_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.21"
@@ -33,6 +36,15 @@ dependencies {
     implementation("io.ktor:ktor-server-sessions:2.3.0")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    implementation("org.postgresql:postgresql:$postgres_version")
+
+    implementation("com.zaxxer:HikariCP:$hikari_version")
 
 
     testImplementation("io.ktor:ktor-server-test-host-jvm")
